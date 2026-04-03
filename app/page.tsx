2bb414e4c188bf92686513ec137242e2f865217d
Florent Cussatlegras <florent.cussatlegras@gmail.com>
2026-04-03 12:44:08 +0200

    fix alignement content landing page

--- app/page.tsx
+++ app/page.tsx
@@ -9,7 +9,7 @@ export default async function Home() {
 
   return (
     <section className="flex items-center h-full justify-center gap-4 bg-white dark:bg-dark-bg w-full p-0 m-0">
-      <div className="flex flex-row w-full h-full items-stretch justify-between">
+      <div className="flex flex-row w-full items-center justify-between">
         <div className="hidden xl:w-1/2 items-center xl:flex justify-center"> 
           {/* Dark */}
           <div className="block">
